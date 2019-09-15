ac5b05b1ae833ac646355c904921b9e6097348ad
shogunishere <41185150+shogunishere@users.noreply.github.com>
2019-09-15 13:12:04 +0200

    Delete ExamplePDFViewer.jsx

--- components/ExamplePDFViewer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
- 
-const ExamplePDFViewer = () => {
-  return (<PDFViewer document={{
-    url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf'
-  }} />);
-}
- 
-export default ExamplePDFViewer
